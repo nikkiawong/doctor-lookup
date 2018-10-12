96ecfa96f5138b6f9f6844a5420e41f7780c4c1d
Nikki Wong <nikkiwong@miami.edu>
2018-10-12 11:09:43 -0700

    clear result field each search and return error message for no results

--- src/main.js
+++ src/main.js
@@ -15,31 +15,39 @@ $(document).ready(function() {
 
     promise.then(function(response){
       let body = JSON.parse(response);
+      console.log(body);
 
-      for(let i=0; i < body.data.length; i++) {
+      if (body.data.length === 0) {
+        $(".results").html("");
+        $(".results").html("<p>Sorry, no results found. Please try searching again.</p>");
+      } else {
+        $(".results").html("");
+        for(let i=0; i < body.data.length; i++) {
 
-        let street2;
-        if (body.data[i].practices[0].visit_address.street2 === undefined) {
-          street2 = "";
-        } else {
-          street2 = body.data[i].practices[0].visit_address.street2 + ", "
-        }
+          let street2;
+          if (body.data[i].practices[0].visit_address.street2 === undefined) {
+            street2 = "";
+          } else {
+            street2 = body.data[i].practices[0].visit_address.street2 + ", "
+          }
 
-        let website;
-        if (body.data[i].practices[0].website === undefined) {
-          website = "";
-        } else {
-          website = `<a href='${body.data[i].practices[0].website}'>Website</a>`;
-        }
+          let website;
+          if (body.data[i].practices[0].website === undefined) {
+            website = "";
+          } else {
+            website = `<a href='${body.data[i].practices[0].website}'>Website</a>`;
+          }
 
-        let acceptsPatients;
-        if (body.data[i].practices[0].accepts_new_patients === true) {
-          acceptsPatients = "Yes";
-        } else {
-          acceptsPatients = "No";
-        }
+          let acceptsPatients;
+          if (body.data[i].practices[0].accepts_new_patients === true) {
+            acceptsPatients = "Yes";
+          } else {
+            acceptsPatients = "No";
+          }
 
-        $(".results").append(`<h2>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</h2><p>${body.data[i].practices[0].visit_address.street + ", " + street2 + body.data[i].practices[0].visit_address.city + ", " + body.data[i].practices[0].visit_address.state + " " + body.data[0].practices[0].visit_address.zip}</p><p>${body.data[i].practices[0].phones[0].number}</p><p>${website}</p><p><span class='bold'>Accepts new patients?</span> ${acceptsPatients}</p>`);
+          $(".results").append(`<h2>${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</h2><p>${body.data[i].practices[0].visit_address.street + ", " + street2 + body.data[i].practices[0].visit_address.city + ", " + body.data[i].practices[0].visit_address.state + " " + body.data[0].practices[0].visit_address.zip}</p><p>${body.data[i].practices[0].phones[0].number}</p><p>${website}</p><p><span class='bold'>Accepts new patients?</span> ${acceptsPatients}</p>`);
+      }
+
       }
     });
   });
