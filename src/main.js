cd6e9c990824fb2097d11d43a7cc8434df990ca6
Nikki Wong <nikkiwong@miami.edu>
2018-10-12 11:55:27 -0700

    add toggle to doctor cards

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
 
     promise.then(function(response){
       let body = JSON.parse(response);
-      console.log(body);
 
       if (body.data.length === 0) {
         $(".results").html("");
@@ -44,13 +43,20 @@ $(document).ready(function() {
           } else {
             acceptsPatients = "No";
           }
-          console.log(street2);
 
-          $(".results").append(`<div class="card" style="width: 35rem;"><div class="card-body"><h2 class="card-title">${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</h2><img src="${body.data[i].profile.image_url}"><p>${body.data[i].practices[0].visit_address.street}</p><p>${street2}</p>
-          <p>${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[0].practices[0].visit_address.zip}</p><p>${body.data[i].practices[0].phones[0].number}</p><p>${website}</p><p><span class='bold'>Accepts new patients?</span> ${acceptsPatients}</p></div></div>`);
+          $(".results").append(`<div class="card" style="width: 35rem;" id="doctor${i}"><div class="card-body"><h2 class="card-title">${body.data[i].profile.first_name} ${body.data[i].profile.last_name}</h2><div class="doctor-info" id="doctor-info-doctor${i}"><img src="${body.data[i].profile.image_url}"><p>${body.data[i].practices[0].visit_address.street}</p><p>${street2}</p>
+          <p>${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[0].practices[0].visit_address.zip}</p><p>${body.data[i].practices[0].phones[0].number}</p><p>${website}</p><p><span class='bold'>Accepts new patients?</span> ${acceptsPatients}</p></div></div></div>`);
         }
 
       }
+
+      $(".card").hover(function() {
+        const cardId = $(this).attr("id");
+        $(`#${cardId}`).click(function() {
+          $(`#doctor-info-${cardId}`).toggle();
+        });
+      });
     });
+
   });
 });
